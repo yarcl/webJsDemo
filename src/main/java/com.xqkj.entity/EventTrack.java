5f9e58825e072f053525942fa1690453e9e41666
zhi.xiao <zhi.xiao@wbkit.com>
2017-09-15 11:44:56 +0800

    change for test

--- src/main/java/com.xqkj.entity/EventTrack.java
+++ src/main/java/com.xqkj.entity/EventTrack.java
@@ -8,7 +8,6 @@ package com.demo.action;
 public class EventTrack {
     private String erId;    //id
     private String eventId; //同event_definition的主键event_id
-    //    private String eventIdentifier; // 事件别名
     private String eventPath;       //xpath路径
     private String activity;
     private String version;
